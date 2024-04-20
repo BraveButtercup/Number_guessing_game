96c523d95afca0d1a19c7566c70e93ebfb345550
bravebuttercup <batorbogi@gmail.com>
2024-04-20 18:20:18 +0200

    Validation changes, input clear after submit, gif added

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let hozzAdGomb = document.querySelector(".js-submit-guess");
 
 let target;
 let listOfGuesses = [];
+let gameover = true;
 
 function generateNumber() {
     return Math.floor(Math.random() * 100) + 1;
@@ -19,21 +20,30 @@ function resetGame() {
 function submitGame(event) {
     event.preventDefault();
     let guessValue = Number.parseInt(beviteliMezo.value);
-    if (!isNaN(guessValue)) {
-        validateGuess();
-        compareGuessToTarget(guessValue)
-        renderList(guessValue)
-    };
+    validateGuess(guessValue);
+    compareGuessToTarget(guessValue);
+    renderList(guessValue);
+
 }
 
-function compareGuessToTarget(guess) {
 
-    if (guess > target) {
-        tippTarolo.innerHTML = `<div class="alert alert-info" role="alert">${guess} - A szám kisebb! </div>`
-    } else if (guess < target) {
-        tippTarolo.innerHTML = `<div class="alert alert-warning" role="alert">${guess} - A szám nagyobb! </div>`
-    } else if (guess === target) {
-        tippTarolo.innerHTML = `<div class="alert alert-success" role="alert">${guess} - Gratulálok!Eltaláltad </div>`
+function compareGuessToTarget(guess) {
+    if (!isNaN(guess) && (guess > 1 && guess < 100)) {
+        if (guess > target) {
+            tippTarolo.innerHTML = `<div class="alert alert-info" role="alert">${guess} - Your guess is higher! </div>`
+
+        } else if (guess < target) {
+            tippTarolo.innerHTML = `<div class="alert alert-warning" role="alert">${guess} - Your guess is lower! </div>`
+
+        } else if (guess === target) {
+            tippTarolo.innerHTML = `
+            <div class="alert alert-success" role="alert">${guess} - Congratulation! You've guessed out! </div>
+            <div class="d-flex justify-content-center"><iframe src="https://giphy.com/embed/Wmtm6YfkV3qXZXunxC" width="200" height="200" frameBorder="0" class=""></iframe></div>
+            `
+        }
+        else {
+            addGuessValidation(guess);
+        }
     }
 };
 function addGuessValidation() {
@@ -53,12 +63,16 @@ function validateGuess() {
         return true;
     }
 }
-function renderList(guessValue) {
-    html = ""
-    listOfGuesses.unshift(guessValue);
-    html += `<div> Az eddigi tippjeid: ${listOfGuesses}</div>`;
-    tippTarolo.innerHTML += html;
-
+function renderList(guess) {
+    if (!isNaN(guess) && (guess > 1 && guess < 100)) {
+        html = ""
+        listOfGuesses.unshift(guess);
+        html += `<div> Your guesses: ${listOfGuesses}</div>`;
+        tippTarolo.innerHTML += html;
+    } else {
+        html += `<div> Invalid number`;
+    }
+    beviteliMezo.value = "";
 }
 
 function startGame() {
@@ -70,3 +84,5 @@ startGame();
 ujJatekGomb.addEventListener('click', startGame);
 hozzAdGomb.addEventListener('click', submitGame);
 beviteliMezo.addEventListener('change', validateGuess);
+
+
